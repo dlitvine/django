9ae94160863e00cddd46b996371cb29cdfb59e12
dlitvine <dlitvine@users.noreply.github.com>
2021-08-08 14:04:09 -0600

    Initial commit

--- StartProject/project/settings.py
+++ StartProject/project/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     'fpages',
 ]
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
